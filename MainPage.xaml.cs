3676dd2cd20655843b17f9cb39bf77d7c1a0af96
Lucassorensenn <153129955+Lucassorensenn@users.noreply.github.com>
2024-03-05 21:03:07 -0700

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,61 @@
+﻿namespace MauiApp5;
+
+public partial class MainPage : ContentPage
+{
+
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+
+
+
+    private void OnFlight (object sender, EventArgs e)
+    {
+        flight.Text = "Flight found";
+    }
+
+
+    //From
+    void OnFromTextChanged(object sender, TextChangedEventArgs e)
+    {
+        string oldText = e.OldTextValue;
+        string newText = e.NewTextValue;
+        string myText = From.Text;
+    }
+
+    void OnFromCompleted(object sender, EventArgs e)
+    {
+        string text = ((Entry)sender).Text;
+    }
+
+    //To
+    void OnToTextChanged(object sender, TextChangedEventArgs e)
+    {
+        string oldText = e.OldTextValue;
+        string newText = e.NewTextValue;
+        string myText = To.Text;
+    }
+
+    void OnToCompleted(object sender, EventArgs e)
+    {
+        string text = ((Entry)sender).Text;
+    }
+
+    //Des
+    void OnDesTextChanged(object sender, TextChangedEventArgs e)
+    {
+        string oldText = e.OldTextValue;
+        string newText = e.NewTextValue;
+        string myText = Des.Text;
+    }
+
+    void OnDesCompleted(object sender, EventArgs e)
+    {
+        string text = ((Entry)sender).Text;
+    }
+
+}
+
